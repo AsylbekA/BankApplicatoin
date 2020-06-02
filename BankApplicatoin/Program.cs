d8c7a1a34e150688e2201b7fadf928a4ee0ba82f
AsylbekA <@anarbayasylbek@gmail.com>
2020-06-03 01:55:51 +0600

    Добавьте файлы проекта.

---
+++ BankApplicatoin/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BankApplicatoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+    }
+}
