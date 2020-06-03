f3babe2a5b358d906efaa13ec5c96bf0f2d7c9f7
AsylbekA <@anarbayasylbek@gmail.com>
2020-06-03 15:56:52 +0600

    ...

--- BankLibrary/Model/DepositAccount.cs
+++ BankLibrary/Model/DepositAccount.cs
@@ -19,6 +19,21 @@ namespace BankLibrary.Model
                 base.Put(sum);
             else
                 base.OnAdded(new AccountEventArgs("На счет можно положить только после 30-ти дневного периода", 0));
+
+        }
+        public override decimal Withdraw(decimal sum)
+        {
+            if (_days%30==0)
+                return base.Withdraw(sum);
+            else
+                base.OnWithdrawed(new AccountEventArgs("Вывести средства можно только после 30-ти дневного периода", 0));
+            return 0;
+        }
+
+        protected internal override void Calculate()
+        {
+            if(_days%30==0)
+                base.Calculate();
         }
     }
 }
